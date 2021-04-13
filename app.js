b9f4cdbc55b4bece836b82c6d86da497ad985316
nick <whiteshadow148@gmail.com>
2021-04-13 13:15:34 -0600

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Howdey Partner'));
+app.get('/', (req, res) => res.send('Howdey Partner You smell funny'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
